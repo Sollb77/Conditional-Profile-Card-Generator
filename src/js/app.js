316d3857daa4c7eacce1e53e8c7b44a18cc7f40d
Sollb77 <qsiecuadorservicios@gmail.com>
2023-01-16 15:49:01 +0000

    Primeros cambios

--- src/js/app.js
+++ src/js/app.js
@@ -30,12 +30,14 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
+  let name = document.getElementById("name");
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
+           
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>"${variables.name + +variables.lastname}"</h1>         
+           <h2>Web Developer</h2>
+          <h3>Miami, USA </h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
