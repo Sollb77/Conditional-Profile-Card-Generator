4bc1ef4a13bee18242be6468a5941e7d5e15a84b
Sollb77 <qsiecuadorservicios@gmail.com>
2023-01-16 21:36:36 +0000

    mod

--- src/js/app.js
+++ src/js/app.js
@@ -30,19 +30,22 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
-  let name = document.getElementById("name");
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-            
-          <a href><img src="./ "  class="photo" /><a href>
+           
+            <button src=${variables.avatarURL} class="photo"></button>
+
+          
+              
                     
-          <h1>${variables.name + " " + variables.lastname}"</h1>        
+          <h1>${variables.name}  ,  ${variables.lastname}</h1>          
           <h2>"${variables.role}"</h2>
           <h3>"${variables.city + "," + variables.country}" </h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${
               variables.twitter
-            }"><i class="fab fa-twitter"></i></a></li>
+            }"><i class="fa-brands fa-twitter"></i></a></li>
             <li><a href="https://github.com/${
               variables.github
             }"><i class="fab fa-github"></i></a></li>
